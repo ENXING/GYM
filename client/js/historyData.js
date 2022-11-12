7e5572f0deb7c55b0a7f80b48076fb0a3d146bcf
ENXING <enxingxiong@gmail.com>
2022-11-12 03:10:44 -0500

    CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.2

--- client/js/historyData.js
+++ client/js/historyData.js
@@ -9,55 +9,28 @@ function clickDelete() {
 }
 
 
-function getHistory() {
-    const xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'http://43.226.26.53:10201/get_history');
-    xhr.open('GET', 'http://workoutreport.ga:10201/get_history');
-    // xhr.open('GET', 'http://localhost:10201/get-history');
-    xhr.send()
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4){
-            if(xhr.status >= 200 && xhr.status < 300){
-                let data = JSON.parse(xhr.response);
-                for (let i = 0; i < data.length; ++i) {
-                    var $insertIten = $('<tr></tr>')
-                    .append('<th scope="row">' + data[i]._id+ '</th>')
-                    .append('<td>' + data[i].exercise.name + '</td>')
-                    .append('<td>' + data[i].repeat+ '</td>')
-                    .append('<td>' + data[i].weight+ '</td>')
-                    .append('<td>' + (new Date(data[i].created))+'</td>')
-                    .append('<td><a >Delete</a></td>')
-                    .appendTo('#exercieseTable>tbody')
-                    console.log(new Date(data[i].created))
-                }
-            }
-        }
-    } 
-}
-{/* <tr>
-<th scope="row">1</th>
-<td>bench press</td>
-<td>3</td>
-<td>45</td>
-<td>19/192/21</td>
-<td><a >Delete</a></td>
-</tr> */}
-
-// $('#addEmpButton').click(function () {
-//     var $empName = $('#empName')
-//     var $email = $('#email')
-//     var $salary = $('#salary')
-//     var empName = $empName.val()
-//     var email = $email.val()
-//     var salary = $salary.val()
-
-//     var $xxx = $('<tr></tr>')
-//         .append('<td>' + empName + '</td>') 
-//         .append('<td>' + email + '</td>')
-//         .append('<td>' + salary + '</td>')
-//         .append('<td><a href="deleteEmp?id="' + Date.now() + '>Delete</a></td>')
-//         .appendTo('#exercieseTable>tbody')
-//     $empName.val('')
-//     $email.val('')
-//     $salary.val('')
-// })
+// function getHistory() {
+//     const xhr = new XMLHttpRequest();
+//     // xhr.open('GET', 'http://43.226.26.53:10201/get_history');
+//     xhr.open('GET', 'http://workoutreport.ga:10201/get_history');
+//     // xhr.open('GET', 'http://localhost:10201/get-history');
+//     xhr.send()
+//     xhr.onreadystatechange = function(){
+//         if(xhr.readyState === 4){
+//             if(xhr.status >= 200 && xhr.status < 300){
+//                 let data = JSON.parse(xhr.response);
+//                 for (let i = 0; i < data.length; ++i) {
+//                     var $insertIten = $('<tr></tr>')
+//                     .append('<th scope="row">' + data[i]._id+ '</th>')
+//                     .append('<td>' + data[i].exercise.name + '</td>')
+//                     .append('<td>' + data[i].repeat+ '</td>')
+//                     .append('<td>' + data[i].weight+ '</td>')
+//                     .append('<td>' + (new Date(data[i].created))+'</td>')
+//                     .append('<td><a >Delete</a></td>')
+//                     .appendTo('#exercieseTable>tbody')
+//                     console.log(new Date(data[i].created))
+//                 }
+//             }
+//         }
+//     } 
+// }
