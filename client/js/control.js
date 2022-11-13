922befcd0c194bdbb6d5af67dfae7b6f603ab1f5
ENXING <enxingxiong@gmail.com>
2022-11-12 23:20:23 -0500

    data graph finished

--- client/js/control.js
+++ client/js/control.js
@@ -3,7 +3,7 @@ $('#history_data').click(e=>{
     if(!event.detail || event.detail == 1){
         present.hide()
         present = $('#history_form')
-        var p1 = new Promise(getHistory);
+        var p1 = new Promise(printHistory);
         p1.then((e)=>present.show(), (e)=>alert(e))
     }
 });
@@ -17,7 +17,7 @@ $('#graph').click(e => {
     if(!event.detail || event.detail == 1){
         present.hide()
         present = $('#graph_form')
-        // showGraph();
+        showGraph(new Date('2022-11-1'));
         present.show()
     }
 });
--- client/js/graph.js
+++ client/js/graph.js
@@ -1,56 +1,79 @@
-// function showGraph() {
-//     const xhr = new XMLHttpRequest();
-//     // xhr.open('GET', 'http://localhost:10201/jsonp-server?');
-//     xhr.open('GET', 'http://' + ip + ':10201/jsonp-server?');
-//     xhr.send()
-//     xhr.onreadystatechange = function(){
-//         if(xhr.readyState === 4){
-//             if(xhr.status >= 200 && xhr.status < 300){
-//                 let data = JSON.parse(xhr.response);
-//                 let x = []
-//                 for (let i = 0; i < data.length; ++i) {
-//                     x.push(i);
-//                 }
-//                 TESTER = document.getElementById('tester');
-//                 Plotly.newPlot(TESTER, [{
-//                     'x': x,
-//                     'y': data 
-//                 }], {
-//                     margin: { t: 0 }
-//                 });
-//             }
-//         }
-//     }
-// };
 
-const labels = [
-    'January'
-  ];
-
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [45],
-    }, 
-    {
-      label: 'My second dataset',
-      backgroundColor: 'rgb(0, 99, 132)',
-      borderColor: 'rgb(0, 99, 132)',
-      data: [5],
-    }
+function showGraph(startDay) {
+  let colors = [
+    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'
   ]
-  };
+  let now = Date.now()
+  let p = new Promise(getAll)
+  let labels_x = getDatesInRange(startDay, new Date())
+  let data = {labels: labels_x, datasets: []}
+  let name2color = {}
+  let count = 0;
+  let record = {}
+  p.then((items)=>{
+    for (let i = 0; i < items.length; ++i) {
+      let name = items[i].exercise.name;
+      let color = colors[count]
+      if (name in name2color) {
+        color = name2color[name]
+      } else {
+        name2color[name] = colors[count]; count++;
+      }
+      let date = new Date(items[i].created).toLocaleDateString("en-US");
+      if (record[name] !== undefined && date in record[name]) {
+        record[name][date].push({
+          weight: items[i].weight,
+          repeat: items[i].repeat
+        })
+      } else {
+        record[name] = {
+          [new Date(items[i].created).toLocaleDateString("en-US")]: [{
+          weight: items[i].weight,
+          repeat: items[i].repeat
+          }]
+        }
+      }
+      // let item = {label: name, backgroundColor: color, borderColor: color, data}
+      console.log(new Date(items[i].created))
+    }
+    for (let label in record) {
+      let item = {label: label, backgroundColor: name2color[label], borderColor: name2color[label], data: Array(labels_x.length).fill()}
+      for (let j = 0; j < labels_x.length; ++j) {
+        let day = labels_x[j];
+        if (day in record[label]) {
+          if (item.data[j] === undefined) item.data[j] = 0;
+          for (let k = 0; k < record[label][day].length; ++k)
+            item.data[j] += record[label][day][k].weight * record[label][day][k].repeat;
+        }
+      }
+      data.datasets.push(item);
+    }
+    console.log(data)
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+    if (Window.myChart_ !== undefined) {
+      Window.myChart_.destroy();
+    }
+    Window.myChart_ = new Chart( document.getElementById('myChart'), config);
+  })
+};
 
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
+
+
+function getDatesInRange(startDate, endDate) {
+  const date = new Date(startDate.getTime());
+
+  const dates = [];
+
+  while (date <= endDate) {
+    dates.push((new Date(date)).toLocaleDateString("en-US"));
+    date.setDate(date.getDate() + 1);
+  }
+
+  return dates;
+}
+
 
