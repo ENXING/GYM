0ff5e3138f0c11cbbab08cd08bab1d57b6df5afb
ENXING <enxingxiong@gmail.com>
2022-11-21 18:43:33 -0500

    test jwt

--- server/db.js
+++ server/db.js
@@ -1,22 +1,22 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const SECRET = config.jwtSecretKey;
 
-var exercise = require("./exercise")
+
+const exercise = require("./exercise")
 mongoose.connect("mongodb://127.0.0.1/exercise", ()=>{
 	console.log("connected>>>>>>>>>>>>")
 }, e=>console.error(e));
 
-async function run(obj) {
+async function insertWorkout(obj) {
 	
 	try{
-		// insert
-		// let actionName = exercise..create({name: obj.exercise, url: 'http://baidu.com'})
 		let record = {exercise: {name: obj.exercise}, weight: Number(obj.weight), repeat: Number(obj.repeat)}
 		console.log(record)
 		const actionName = new exercise.Workout(record)
 		await actionName.save()
-
 		console.log(actionName)
-		// const workout = new exercise.Workout({exercise: new exercise.ActionName({name: "pull up"}), })
 	} catch(e) {
 		console.log(e.message)
 	}
@@ -37,8 +37,6 @@ const app = express();
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-
 app.use(express.json())
 
 app.post('/api/del-item', urlencodedParser, (request, response) => {
@@ -52,18 +50,78 @@ app.post('/api/del-item', urlencodedParser, (request, response) => {
 	})
 });
 
-app.post('/api/test', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-	console.log(req)
-	res.send("test!")
-}) 
+
+app.get('/api/users', async (req, res) => {
+    const users = await exercise.User.find();
+    res.send(users)
+});
+app.post('/api/register', async (req, res) => {
+    // console.log(req.body);
+    let user;
+    try{
+        user = await exercise.User.create({
+            username: req.body.username,
+            password: req.body.password
+        })
+    } catch (e) {
+        res.status(409).send(e)
+        return
+    }
+    console.log("inserted>>>>>>")
+    res.send(user);
+});
+
+app.post('/api/login', async (req, res) => {
+    // console.log(req.body);
+    const user = await exercise.User.findOne({
+        username: req.body.username
+    })
+    if(!user){
+        return res.status(422).send({
+            message: 'wrong'
+        });
+    }
+    const isPasswordValid = require('bcrypt').compareSync(
+        req.body.password,
+        user.password
+    )
+    if (!isPasswordValid) {
+        return res.status(401).send({
+            message: 'wrong'
+        })
+    };
+    //生成token
+    const token = jwt.sign({
+        id: String(user._id),//密码不要放进来，放一个唯一的东西就可以了
+    }, SECRET, {expiresIn: 20});
+
+    res.send({
+        user,
+        token
+    });
+});
+const auth = async (req, res, next) => {
+    try {
+        const raw = String(req.headers.authorization).split(' ').pop();
+        const { id } = jwt.verify(raw, SECRET);
+        req.user = await exercise.User.findById(id);//这里需要添加一些错误处理，不执行next
+    } catch (e) {
+        res.status(402).send(e);
+        console.log(e)
+        return
+    }
+    next();
+}
+
+app.get('/api/profile', auth, async (req, res) => {
+    res.send(req.user);
+})
 
 app.post('/api/put-workout', urlencodedParser, (request, response) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
     let str = JSON.stringify(request.body);
-	run(request.body)
+	insertWorkout(request.body)
 	response.send(request.body)
 });
 
@@ -79,26 +137,6 @@ app.get('/api/get-history', (request, response)=>{
 		response.send({})
 		console.log("error query find in mongo")
 	})
-});
-
-
-app.all('/api/jsonp-server', (request, response)=>{
-	response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Headers', '*');
-	let x =[] 
-	for (let i = 0; i < 100; i++) x.push(i)
-	function between(min, max) {
-		return Math.floor(Math.random() * (max-min) + min)
-	}
-	let data = x.map((key)=> Math.random(10,10 + key))
-
-	let str = JSON.stringify(data)
-	response.send(str);
-});
-
-app.get('/api/server', (request, response) => {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.send('HELLO AJAX - 2 express');
 });
 
 app.listen(10201, () => {
