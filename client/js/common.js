aaf470bfc14f219ea2150989ffd10cc605c72dce
ENXING <enxingxiong@gmail.com>
2022-11-23 00:59:02 -0500

    muti user -login register logout2

--- client/js/common.js
+++ client/js/common.js
@@ -5,6 +5,6 @@ function json2url(obj) {
     }
     return ret.slice(0, -1);
 }
-var url = 'http://localhost:10201/api';
-// var url = 'https://workout.enxing.cf/api';
+// var url = 'http://localhost:10201/api';
+var url = 'https://workout.enxing.cf/api';
 sessionStorage.setItem("url", url);
